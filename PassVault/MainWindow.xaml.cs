70627a6f40875cab23b06d6ed959a593990117e0
Yan Nirman <nirman.yan@gmail.com>
2020-08-09 10:23:13 +0000

    random double

---
+++ PassVault/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Security.Cryptography;
+
+
+
+namespace PassVault
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            byte[] randomNumber = new byte[8];
+            rngCsp.GetBytes(randomNumber);
+            RandomNumber.Text = BitConverter.ToDouble(randomNumber, 0).ToString();
+        }
+
+    }
+}
